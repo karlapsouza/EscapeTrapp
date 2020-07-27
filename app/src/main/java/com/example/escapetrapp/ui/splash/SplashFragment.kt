369f41427b06f4b7d7bce3ed5d8b1bdb76d996a2
Karla <karlapsouza@hotmail.com>
2020-07-27 09:48:47 -0300

    adicionando tela de criaç±ao de usuário, signup

--- app/src/main/java/com/example/escapetrapp/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/escapetrapp/ui/splash/SplashFragment.kt
@@ -1,24 +1,28 @@
 package com.example.escapetrapp.ui.splash
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
 
 import com.example.escapetrapp.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class SplashFragment : Fragment() {
 
+class SplashFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+        NavHostFragment.findNavController(this) .navigate(R.id.action_splashFragment_to_login_nav_graph)
+    }, 2000) }
 }
