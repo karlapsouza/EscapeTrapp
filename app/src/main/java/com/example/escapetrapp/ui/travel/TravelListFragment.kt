f5245e319cd16dadba439c6704cad4c4b1cf0ab2
Karla <karlapsouza@hotmail.com>
2020-08-15 06:08:47 -0300

    criando telas de listagem e cadastro de viagem

---
+++ app/src/main/java/com/example/escapetrapp/ui/travel/TravelListFragment.kt
@@ -0,0 +1,17 @@
+package com.example.escapetrapp.ui.travel
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.escapetrapp.R
+import com.example.escapetrapp.base.BaseFragment
+
+class TravelListFragment : BaseFragment() {
+    override val layout = R.layout.fragment_travel_list
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
