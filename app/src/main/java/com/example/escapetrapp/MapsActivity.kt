72a71b9e2f0deee244e0fed8ccca1114c26ea3ed
Karla <karlapsouza@hotmail.com>
2020-09-03 19:22:32 -0300

    Apresentando mapa de tela de cadastro de viagem

--- app/src/main/java/com/example/escapetrapp/MapsActivity.kt
+++ app/src/main/java/com/example/escapetrapp/MapsActivity.kt
@@ -1,64 +1,40 @@
 package com.example.escapetrapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException
-import com.google.android.gms.common.GooglePlayServicesRepairableException
-import com.google.android.gms.location.places.ui.PlacePicker
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
-    companion object{
-        private const val PLACE_PICKER_REQUEST = 3
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+//        mMap = googleMap
+//        val destination = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(destination).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(destination, 10f))
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,10f))
-    }
-
-    private fun loadPlacePicker() {
-        val builder = PlacePicker.IntentBuilder()
-
-        try {
-            startActivityForResult(builder.build(this@MapsActivity), PLACE_PICKER_REQUEST)
-        } catch (e: GooglePlayServicesRepairableException) {
-            e.printStackTrace()
-        } catch (e: GooglePlayServicesNotAvailableException) {
-            e.printStackTrace()
-        }
+        val query = "pontos turísticos"
+        val zoom = 10
+        val geo = "geo:0,0?q=$query&z=$zoom"
+        val geoUri = Uri.parse( geo )
+        val intent = Intent( Intent.ACTION_VIEW, geoUri )
+        intent.setPackage( "com.google.android.apps.maps" )
+        startActivity( intent )
     }
 
 }
