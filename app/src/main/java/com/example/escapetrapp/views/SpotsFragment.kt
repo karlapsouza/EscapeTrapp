edb5bfa24552411426f1f6edc5314017fc7914e9
Karla <karlapsouza@hotmail.com>
2020-09-03 10:05:08 -0300

    Criando tela de pontos turisticos

---
+++ app/src/main/java/com/example/escapetrapp/views/SpotsFragment.kt
@@ -0,0 +1,14 @@
+package com.example.escapetrapp.views
+
+import android.os.Bundle
+import android.view.View
+import com.example.escapetrapp.R
+import com.example.escapetrapp.base.BaseFragment
+
+class SpotsFragment : BaseFragment() {
+    override val layout = R.layout.fragment_splash
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
